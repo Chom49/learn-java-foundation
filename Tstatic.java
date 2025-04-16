c8e5412f0f099db50a67341df12faa9384d4100a
Chom49 <kom29062003@gmail.com>
2025-04-16 08:03:06 +0300

    first commit

---
+++ Tstatic.java
@@ -0,0 +1,39 @@
+
+class Mobile
+{
+    private int price=9;
+    private String name="khadija";
+    private String brand="smartphone";
+
+    
+    public String getName()
+    {
+       return name;
+    }
+
+    public int getprice()
+    {
+        return price;
+    }
+    public String getbrand()
+    {
+       return brand;
+    }
+
+
+    
+}
+
+
+public class Tstatic {
+    public static void main(String[] args) {
+        Mobile obj=new Mobile();
+   
+System.out.println( obj.getName()+" "+   obj.getprice()+" "+   obj.getbrand());
+
+
+
+    }
+
+    
+}
